030c4a2a06f120f9ef3a0272c239ed4946596e0c
AshleeGarcia <ashleegarcia.dev@gmail.com>
2025-09-05 21:24:03 -0500

    Reemplazar View por SafeAreaView en App.js y agregar componente RegistroForm

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import RegistroForm from './src/components/RegistroForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <RegistroForm />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
